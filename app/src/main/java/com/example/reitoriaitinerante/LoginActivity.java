c35e090905c775299ff5745baf75e782c61693cf
Kaylane <kaylanni.fernandes@academico.ifpb.edu.br>
2024-05-27 12:44:15 -0300

    Criação da activity "activity_login"

---
+++ app/src/main/java/com/example/reitoriaitinerante/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.reitoriaitinerante;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView signInWithGoogle;
+    private GoogleSignInAccount usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        signInWithGoogle = findViewById(R.id.signInWithGoogle);
+        signInWithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
