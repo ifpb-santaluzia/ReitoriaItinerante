c7c8c8f9ac28ce1c7782f0d3ae0201c133ac2d04
tuliosergio2 <tulio.goncalves@academico.ifpb.edu.br>
2024-05-29 11:45:49 -0300

    atualizacao da nomenclatura na barra lateral e toolbar

--- app/src/main/java/com/example/reitoriaitinerante/MainActivity.java
+++ app/src/main/java/com/example/reitoriaitinerante/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.reitoriaitinerante;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -45,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_menu_inicial, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
