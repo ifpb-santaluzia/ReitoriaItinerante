3b6f36146eb19615b36303e4927e22c111abd647
Kaylane <kaylanni.fernandes@academico.ifpb.edu.br>
2024-07-02 19:01:06 -0300

    Merge da activity "Sobre" a brench "Desenvolvimento"

---
+++ app/src/main/java/com/example/reitoriaitinerante/Sobre.java
@@ -0,0 +1,52 @@
+package com.example.reitoriaitinerante;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Sobre extends AppCompatActivity {
+
+    public TextView kaylanniTextView;
+    public TextView gabrielTextView;
+    public TextView tulioTextView;
+    public TextView pedroTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sobre);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        getSupportActionBar().hide();
+        kaylanniTextView = findViewById(R.id.kaylanniTextView);
+        kaylanniTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        kaylanniTextView.setLinkTextColor(Color.GRAY);
+
+        tulioTextView = findViewById(R.id.tulioTextView);
+        tulioTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        tulioTextView.setLinkTextColor(Color.GRAY);
+
+        gabrielTextView = findViewById(R.id.gabrielTextView);
+        gabrielTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        gabrielTextView.setLinkTextColor(Color.GRAY);
+
+        pedroTextView = findViewById(R.id.pedroTextView);
+        pedroTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        pedroTextView.setLinkTextColor(Color.GRAY);
+    }
+
+
+}
