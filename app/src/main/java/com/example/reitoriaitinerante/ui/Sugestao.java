ead3f814e90b11906997d25ae89336b3d550caaa
ALUNOS <sergiotulio146@gmail.com>
2024-08-22 16:56:19 -0300

    Banco de dados

--- app/src/main/java/com/example/reitoriaitinerante/ui/Sugestao.java
+++ app/src/main/java/com/example/reitoriaitinerante/ui/Sugestao.java
@@ -6,7 +6,6 @@ public class Sugestao implements Serializable {
     private String sugestao;
     private String topico;
     private boolean anonimo;
-    private Aluno aluno;
 
     public Sugestao(String sugestao, String topico, boolean anonimo) {
         this.sugestao = sugestao;
